a4967b1bda0d7007131e50bfc97bacf49996d4cf
VladBoisa <60569670+vladboisa@users.noreply.github.com>
2020-05-14 16:37:58 +0300

    asggas

--- src/js/filmresults.js
+++ src/js/filmresults.js
@@ -11,9 +11,9 @@ export async function showResults(results) {
     mySwiper.removeAllSlides();
     results.forEach(movie => {
       mySwiper.appendSlide([`<div class="swiper-slide"><div class="card text-white bg-dark">
-        <a href="https://www.imdb.com/title/${movie.imdbID}"><img class="card-img-top" src=${movie.Poster==='N/A'? 'img/image-not-found.gif':movie.Poster} width="295" height="440" alt="${movie.Title}" onerror="this.src = 'img/image-not-found.gif' "></a>
+        <a href="https://www.imdb.com/title/${movie.imdbID}" target="_blank"><img class="card-img-top" src=${movie.Poster==='N/A'? 'img/image-not-found.gif':movie.Poster} width="295" height="440" alt="${movie.Title}" onerror="this.src = 'img/image-not-found.gif' "></a>
             <div class="card-body">
-              <h5 class="card-title text-center"><a href="https://www.imdb.com/title/${movie.imdbID}/videogallery" class="card-title">${movie.Title}</a></h5>
+              <h5 class="card-title text-center"><a href="https://www.imdb.com/title/${movie.imdbID}/videogallery" target="_blank" class="card-title">${movie.Title}</a></h5>
                 <p class="card-text text-center">${movie.Year}</p>
             </div>
         </div>`])
